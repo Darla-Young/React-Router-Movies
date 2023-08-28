4a16e6d7c9b47f612b731f91ad0bf39b0a54fce2
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-08-28 15:22:38 -0500

    complete except stretch

--- src/Movies/MovieCard.js
+++ src/Movies/MovieCard.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function MovieCard (props) {
-  return;
+  const navigate = useNavigate();
+  // console.log(props.movie);
+  const { id, title, director, metascore } = props.movie;
+
+  return (
+    <div className="movie-card" onClick={() => navigate(`/movie/${id}`)} >
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+    </div>
+  );
 }
